567c56ee59d49be1b6c1e5bee3ea6cd29b300e7a
Fisher <domenikfail@gmail.com>
2019-09-16 19:02:47 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -112,4 +112,116 @@ window.addEventListener("DOMContentLoaded", function(){
         descriptionBtn[i].addEventListener('click', open);
     }
 
+
+   //form
+
+
+
+    let message = {
+        loading: "Загрузка...",
+        sucsess: "Спасибо! Скоро мы с Вами свяжемся!",
+        failure: "Что-то пошло не так..."
+    };
+    
+    let form = document.querySelector('.main-form'),
+        formDown = document.querySelector('#form'),
+        input = form.getElementsByTagName ('input'),
+        statusMessage = document.createElement('div');
+        
+    
+        statusMessage.classList.add('status');
+    
+    function sendForm(elem){
+    
+        elem.addEventListener('submit', function (event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+        
+            let formData = new FormData(elem);
+        
+        function postData() {
+            return new Promise(function (resolve, reject) {
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+                request.onreadystatechange = function () {
+                    if(request.readyState < 4) {
+                        resolve();
+                    } else if (request.readyState === 4) {
+                        if (request.status == 200 && request.status < 300) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    }
+                };
+                request.send(formData); // Вот тут другое название переменной
+            });
+        }
+            function clearInput() {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
+        postData (formData)
+            .then (() => statusMessage.innerHTML = message.loading)
+            .then (() => statusMessage.innerHTML = message.sucsess)
+            .catch (() => statusMessage.innerHTML = message.failure)
+            .then (clearInput);
+        });
+    }
+    sendForm(form);
+    sendForm(formDown);
+
+    //SLIDER
+
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);    
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1; 
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    });
 });
+
+
